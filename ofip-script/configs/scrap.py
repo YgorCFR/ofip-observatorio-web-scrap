2a71421c6071ca73c8e9be0ffbcb59841b673632
YgorCFR <ygor.rolim96@gmail.com>
2020-01-28 23:04:04 -0300

    Construindo tratamento de scrap de novos sites.

--- ofip-script/configs/scrap.py
+++ ofip-script/configs/scrap.py
@@ -9,7 +9,6 @@ op.add_argument('log-level=3')
 driver = webdriver.Chrome("chromedriver.exe", options=op)
 
 
-
 # # url = 'https://www.sulbahianews.com.br/abertas-as-inscricoes-para-selecao-de-projetos-de-pesquisa-contra-a-tuberculose/'
 # # with open("webSitesInfo.json") as file:
 # #     data = json.load(file)
@@ -48,17 +47,21 @@ driver = webdriver.Chrome("chromedriver.exe", options=op)
 # # print(texto)
 
 # driver.get('https://g1.globo.com/rj/rio-de-janeiro/noticia/2020/01/20/witzel-diz-acreditar-que-a-situacao-da-cedae-foi-sabotagem.ghtml')
+# driver.get('https://valor.globo.com/financas/noticia/2020/01/27/aversao-ao-risco-com-coronavirus-derruba-bolsa-e-leva-dolar-a-r-422.ghtml')
+# driver.get('https://oglobo.globo.com/sociedade/saude/coronavirus-que-se-sabe-ate-agora-24204619')
+# driver.get('https://brasil.elpais.com/internacional/2020-01-26/china-avisa-que-a-capacidade-de-contagio-do-coronavirus-se-torna-mais-forte.html')
+content = driver.page_source
 
-# content = driver.page_source
+soup = BeautifulSoup(content, features='lxml')
 
-# soup = BeautifulSoup(content, features='lxml')
-
-# ''' FONTE 4 ''' # g1.globo.com
+# ''' FONTE 4 ''' # g1.globo.com #valor.globo.com/ 
 # texto = ''
 # try:
 #     for a in soup.findAll('p', attrs={'class': 'content-text__container'}):
 #         texto = texto + str(a)
 #     texto = texto.replace("<p class=\"content-text__container\" data-track-category=\"Link no Texto\" data-track-links=\"\">", "").replace("</p>", "").replace("<p class=\"content-text__container\">","").replace("<a>","").replace("</a>","")
+#     if texto.count('<p class=\"content-text__container theme-color-primary-first-letter" data-track-category="Link no Texto" data-track-links="">') > 0:
+#         texto = texto.replace("<p class=\"content-text__container theme-color-primary-first-letter\" data-track-category=\"Link no Texto\" data-track-links=\"\">", "")
 #     # texto = texto.replace(texto[texto.index('<a'):texto.index('ghtml\">')+ len('ghtml\">')], "")
 #     while texto.__contains__('<a') == True:
 #         texto = texto.replace(texto[texto.index('<a'):texto.index('ghtml\">')+ len('ghtml\">')], "")
@@ -67,6 +70,23 @@ driver = webdriver.Chrome("chromedriver.exe", options=op)
 #     print(texto)
 # except Exception as ex:
 #     raise Exception(ex)
+
+''' FONTE 5 '''  #brasil.elpais.com/
+texto = ''
+try:
+    for a in soup.findAll('p', attrs={'class':''}):
+        texto = texto + str(a)
+    while texto.__contains__('<a') == True:
+        texto = texto.replace(texto[texto.index('<a'):texto.index('_blank\">')+ len('_blank\">')], "")
+    while texto.__contains__('</a>') == True:
+        texto = texto.replace("</a>","")
+    while texto.__contains__('<p') == True:
+        texto = texto.replace(texto[texto.index('<p'):texto.index('class=\"\">')+ len('class=\"\">')], "")
+    while texto.__contains__('</p>') == True:
+        texto = texto.replace('</p>', "")
+    print(texto)
+except Exception as ex:
+    raise Exception(ex)
 
 # '''FONTE 1'''
 # # r = str(soup).count('element ordem_')
