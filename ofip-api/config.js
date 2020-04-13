283ea6a2013856e11fc137374f5ed59fb39e86f1
Ygor Rolim <ygor.rolim96@gmail.com>
2020-04-13 18:11:02 -0300

    Update config.js

--- ofip-api/config.js
+++ ofip-api/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 3000,
-    dbConectionString: 'mysql://root:750qwe700kxy@@localhost:3306/ofip',
+    dbConectionString: 'mysql://root:**********@localhost:3306/ofip',
     saltRounds: 2,
     jwtSecret: 'secretKey',
     tokenExpireTime: '6h'
