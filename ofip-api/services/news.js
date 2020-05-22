b57d3cd79d88b8e28a04a5d1a66970a9b9fb72ee
YgorCFR <ygor.rolim96@gmail.com>
2020-05-22 01:39:29 -0300

    Adicionando serviço de noticias

---
+++ ofip-api/services/news.js
@@ -0,0 +1,9 @@
+const model = require('../models/index');
+
+// Getting models that are will be used to do the queries
+const Users = model.User;
+const KeyWords = model.KeyWord;
+const News = model.News;
+const Source = model.Source;
+
+
