9e99c0451c7d1f3d54a22aec5c773bccb0e291a4
YgorCFR <ygor.rolim96@gmail.com>
2020-05-22 01:43:18 -0300

    Ajustando arquivo index

--- ofip-api/index.js
+++ ofip-api/index.js
@@ -1,16 +1,38 @@
 const express = require('express');
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const helmet = require('helmet');
+const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const config = require('./config');
+const config = require('./config/config');
 const router = require('./router');
-
-
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+app.set('view engine', 'pug');
+app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended : true
 }));
-app.use(express.static('client'));
+
+const directoryToServe = 'client';
+
+app.use('/' ,express.static(path.join(__dirname, '..', directoryToServe)));
 router.set(app);
 
-app.listen(config.port, () => console.log('App listening on port ' + config.port));
+const httpsOptions = {
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'server.crt')),
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'server.key'))
+};
+
+app.use('/assets', express.static('assets'))
+
+
+https.createServer(httpsOptions, app) 
+    .listen(config.port, function() {
+        console.log(`Serving the ${directoryToServe}/ directory at https://localhost:${config.port}`);
+    });
