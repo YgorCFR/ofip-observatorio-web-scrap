4aa8f192e9a9f4da464acf36ea0d8d4f2a185249
YgorCFR <ygor.rolim96@gmail.com>
2020-05-22 01:46:20 -0300

    Alimentando router.js

--- ofip-api/router.js
+++ ofip-api/router.js
@@ -1,6 +1,24 @@
 const authController = require('./controllers/auth');
+const projectController = require('./controllers/project');
 
-module.exports.set = app => {
-    app.post('/login', authController.login);
-    app.post('/register', authController.register);    
+const authMiddleware = require('./middlewares/auth');
+
+const routes = require('./utils/routes_util');
+
+let route = routes.getServerRoutes();
+let main = route.routes;
+let root = `${route.root.api}${route.root.version}`;
+
+module.exports.set = (app) => {
+    app.post(`${root}${main.login.route}`, authController.login);
+    app.post(`${root}${main.register.route}`, authController.register);
+    app.get(`${root}${main.logout.route}`, authMiddleware.checkAuth, authMiddleware.checkIfTokenIsFromBlackList, authController.logout);
+    app.get(`${root}${main.forgot_password.route}`, authController.forgotPassword);
+    app.post(`${root}${main.change_password.route}`, authController.changePassword);
+    
+    app.get(`${root}${main.reset_password.route}`, authController.renderResetPassword);
+
+    app.get(`${root}${main.get_projects.route}`, authMiddleware.checkAuth, authMiddleware.checkIfTokenIsFromBlackList, authMiddleware.hasRole(`${main.get_projects.name}`) ,projectController.getProjects);
+    app.get(`${root}${main.get_project.route}`, authMiddleware.checkAuth, authMiddleware.checkIfTokenIsFromBlackList, projectController.getProject);
+    app.post(`${root}${main.register_project}`, authMiddleware.checkAuth, authMiddleware.checkIfTokenIsFromBlackList, projectController.addProject);    
 };
