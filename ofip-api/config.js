416fb4e6b3028b3868f57a72abbb631b2270b6b3
YgorCFR <ygor.rolim96@gmail.com>
2020-05-22 01:42:24 -0300

    Ajustando config

--- ofip-api/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    port: 3000,
-    dbConectionString: 'mysql://root:750qwe700kxy@@localhost:3306/ofip',
-    saltRounds: 2,
-    jwtSecret: 'secretKey',
-    tokenExpireTime: '6h'
-};
