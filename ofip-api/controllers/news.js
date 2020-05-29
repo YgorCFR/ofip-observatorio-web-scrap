3b0544d07b1e5c2b87c463c7462797055688588a
YgorCFR <ygor.rolim96@gmail.com>
2020-05-28 23:38:40 -0300

    Adicionando serviços de edição de noticias, exportação para excel e txt

--- ofip-api/controllers/news.js
+++ ofip-api/controllers/news.js
@@ -1,6 +1,10 @@
 const config = require('../config/config');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const excel = require('exceljs');
+const dateUtil = require('../utils/date');
+const table = require('table').table;
+const fs = require('fs');
 
 const newsService = require('../services/news');
 
@@ -85,10 +89,108 @@ const deleteNews = async (req, res, next) => {
             )
 }
 
+const editNews = async (req, res, next) => {
+    return await newsService.editNews(req, res, next).then(
+        news => {
+            res.send({
+                success: true,
+                message: 'Updated.',
+                data: { news }
+            })
+        }
+    ).catch(
+        err => {
+            res.status(400).send({
+                success: false,
+                message: err
+            })
+        })
+}
+
+const exportNewsToExcel = (req, res, next) => {
+    return newsService.mapNewsContent(req, res, next).then(
+        news => {
+            let workbook = new excel.Workbook();
+            let worksheet = workbook.addWorksheet('Analise notícias');
+
+            worksheet.columns = [
+                {header: 'Código', key: 'id', width: 10},
+                {header: 'Título', key: 'titulo', width: 40},
+                {header: 'Data', key: 'data', width: 20},
+                {header: 'Texto', key: 'texto', width: 200},
+                {header: 'Url', key: 'url', width: 30},
+                {header: 'Palavra Chave', key: 'palavra_chave', width: 20},
+                {header: 'Site', key: 'site', width: 20},
+                {header: 'Projeto', key: 'projeto', width: 20}
+            ];
+
+            worksheet.addRows(news);
+
+            let fileName = `extracao_noticias_${dateUtil.makeCurrentDateByCorrectTimeZone(new Date)}_.xlsx`;
+
+            // res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            // res.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+
+            res.attachment(fileName);
+
+            workbook.xlsx.write(res).then(function(){
+                res.end();
+            });
+        },
+        err => {
+            res.status(500).send({
+                success: false,
+                message: `Error with the exportation process: ${err}`
+            })
+        } 
+    )
+}
+
+
+const exportNewsToTxt = (req, res, next) => {
+    return newsService.mapNewsContentTabular(req, res, next).then(
+        news => {
+            let config = {
+                columnDefault: {
+                    width: 20
+                },
+                columnCount: 8,
+                columns: {
+                    0 : { width: 10, alignment: 'left'},
+                    1 : { width: 40, alignment: 'center'},
+                    2 : { width: 20, alignment: 'left'},
+                    3 : { width: 200, alignment: 'left'},
+                    4 : { width: 30, alignment: 'left'},
+                    5 : { width: 20, alignment: 'left'},
+                    6 : { width: 20, alignment: 'left'},
+                    7 : { width: 20, alignment: 'left'},
+                }
+            }
+
+            let output = table(news, config);
+            let fileName = `extracao_noticias_${dateUtil.makeCurrentDateByCorrectTimeZone(new Date)}_.txt`;
+            
+            res.setHeader('Content-type', "application/octet-stream");
+
+            res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
+            res.send(output);
+        },
+        err => {
+            res.status(500).send({
+                success: false,
+                message: `Error with the exportation process: ${err}`
+            })
+        }
+    )
+}
+
 
 module.exports = {
     getNewsHeader,
     getNewsDetail,
     createNews,
-    deleteNews
+    deleteNews,
+    editNews,
+    exportNewsToExcel,
+    exportNewsToTxt
 }
