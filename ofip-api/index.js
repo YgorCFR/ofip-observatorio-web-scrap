612be650ed6525280e3362d2545dc1c630e25040
YgorCFR <ygor.rolim96@gmail.com>
2020-06-01 23:15:59 -0300

    Configurando cors ao projeto

--- ofip-api/index.js
+++ ofip-api/index.js
@@ -31,6 +31,12 @@ const httpsOptions = {
 
 app.use('/assets', express.static('assets'))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 https.createServer(httpsOptions, app) 
     .listen(config.port, function() {
