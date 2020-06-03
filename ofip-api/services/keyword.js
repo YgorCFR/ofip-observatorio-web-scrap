27988b4cd3b909be767c2a17f21736071ec43cd4
YgorCFR <ygor.rolim96@gmail.com>
2020-06-03 00:27:34 -0300

    Adicionando servico de cadastro de palavra chave

--- ofip-api/services/keyword.js
+++ ofip-api/services/keyword.js
@@ -2,8 +2,8 @@ const model = require('../models/index');
 
 
 const KeyWords = model.KeyWord;
-
-
+const Projects = model.Project;
+const sequelize = model.sequelize;
 
 const getAllKeyWords = (req, res, next) => {
     return KeyWords.findAll({
@@ -23,6 +23,63 @@ const getAllKeyWords = (req, res, next) => {
 };
 
 
+const registerKeyWords = async (req, res, next) => {
+    // Verificando a existência do projeto
+    let project = await Projects.findOne({
+        where: {
+            id: req.body.projectId
+        }
+    }).then(project => { return project }).catch(err => { throw new Error('Please provide the project id.') });
+    
+    if (!project) {
+        throw new Error('Project not found, select an existed project to create the keyword.');
+    }
+
+    let countKeyWords = await KeyWords.count({
+        distinct: true,
+        col: 'id'
+    }).then(count => { return count}).catch(() => { throw new Error(`An happened error while counting the keywords. ${error}.`)});
+
+    if (countKeyWords > 99) {
+        throw new Error('You can not register keywords anymore, it reached 100 records.');
+    }
+
+    let checkIfKeywordExists = await KeyWords.count({
+        where: { 
+            value: req.body.value 
+        }
+    }).then(keyword => { return keyword }).catch(() => {throw new Error(err)});
+
+    let result = await sequelize.transaction(async (t) => {
+    
+        if (checkIfKeywordExists == 0) {
+            const keyword = await KeyWords.create({
+                value: req.body.value,
+                projectId: req.body.projectId
+            }, {transaction: t});
+
+            return keyword;
+        }
+        else {
+            throw new Error('The register already exists.');
+        }
+
+    });
+
+    return new Promise(
+        function(resolve, reject) {
+            resolve(result);
+        }
+    ).then(result => {
+        return result;
+    }).catch(err=> {
+        console.log(err);
+        throw new Error(err);
+    })
+}
+
+
 module.exports  = {
-    getAllKeyWords    
+    getAllKeyWords,
+    registerKeyWords    
 }
